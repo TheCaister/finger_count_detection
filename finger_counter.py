76b5dbd7c296fe50c0aba6a40869ef7f81e299f9
Edison Cai <edisoncai2001@gmail.com>
2021-06-23 15:01:52 +0100

    importing finger images to be shown to compare to the hand in the webcam

--- finger_counter.py
+++ finger_counter.py
@@ -2,13 +2,20 @@ import cv2
 import time
 import os
 
+# Setting up webcam
 width_camera, height_camera = 640, 480
 
 cap = cv2.VideoCapture(0)
 cap.set(3, width_camera)
 cap.set(4, height_camera)
 
+# Storing the finger images in a list
+folder_path = "finger_numbers"
+my_list = os.listdir(folder_path)
+print(my_list)
+
 while True:
+    # For displaying webcam
     success, img = cap.read()
     cv2.imshow("Image", img)
     cv2.waitKey(1)
