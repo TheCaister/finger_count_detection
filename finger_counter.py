f48c6ab60c80efaf71a72a75f133e57ba158a8a2
Edison Cai <edisoncai2001@gmail.com>
2021-06-24 15:06:34 +0100

    Making different code for thumb and printing number of fingers up.
    Small glitch: Images not corresponding with numbers

--- finger_counter.py
+++ finger_counter.py
@@ -28,6 +28,7 @@ tip_ids = [4, 8, 12, 16, 20]
 while True:
     # For displaying webcam
     success, img = cap.read()
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
 
     # Find hands and draw them
     img = detector.find_hands(img)
@@ -36,15 +37,40 @@ while True:
     landmarks_list = detector.find_position(img, draw=False)
     # print(landmarks_list)
 
+    # Only process and draw things if landmarks and hands are detected
     if len(landmarks_list) != 0:
-        for id in range(0, 5):
-            # Get the y value of fingertips and their corresponding lower knuckles
-            if landmarks_list[tip_ids[id]][2] < landmarks_list[tip_ids[id] - 2][2]:
-                print("Index finger up")
+        fingers = []
 
-    overlay_list[0] = cv2.resize(overlay_list[0], (200, 200))
-    height, width, channel = overlay_list[0].shape
-    img[0:height, 0:width] = overlay_list[0]
+        # Get the x value of thumb tips and their corresponding lower knuckles
+        # Code for the thumb is different since it's not like the other fingers
+        # If the tip is to the right of the lower knuckle, we can say the thumb is up
+        # Have yet to check for handedness
+        if landmarks_list[tip_ids[0]][1] > landmarks_list[tip_ids[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        for id in range(1, 5):
+            # Get the y value of fingertips and their corresponding lower knuckles
+            # If the tip is above the lower knuckle, we can say it's up
+            # Then append it to the fingers list
+            if landmarks_list[tip_ids[id]][2] < landmarks_list[tip_ids[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        # print(fingers)
+        # Find how many ones are in the fingers list
+        total_fingers = fingers.count(1)
+
+        # Display the appropriate finger image
+        overlay_list[total_fingers - 1] = cv2.resize(overlay_list[total_fingers - 1], (200, 200))
+        height, width, channel = overlay_list[total_fingers - 1].shape
+        img[0:height, 0:width] = overlay_list[total_fingers - 1]
+
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(total_fingers), (45, 375), cv2.FONT_HERSHEY_SIMPLEX,
+                    10, (255, 0, 0), 25)
 
     # Calculating FPS
     current_time = time.time()
